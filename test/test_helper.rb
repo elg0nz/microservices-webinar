d4900771047946f20068fe28e3181ddd20f7fe87
Gonzalo Maldonado <elg0nz@gmail.com>
2017-02-01 21:06:50 -0800

    Add Webmock Dep

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'fake_braintree'
+require 'webmock/test_unit'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
   FakeBraintree.activate!
 end
